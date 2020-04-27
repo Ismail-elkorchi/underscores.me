837805841a725b1652acd3a2293bb00c7d9ec788
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-27 21:42:56 +0000

    Remove hard coded title tag and add theme support

--- functions.php
+++ functions.php
@@ -44,6 +44,14 @@ function underscoresme_setup() {
 	 */
 	load_theme_textdomain( 'underscoresme', get_template_directory() . '/languages' );
 
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
 	/**
 	 * Enable support for Post Thumbnails
 	 */
