ef58f2c09a774c81fbb1f72079bd417ce75f5553
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-27 01:13:19 +0000

    Remove jQuery dependencie

--- js/underscores-scripts.js
+++ js/underscores-scripts.js
@@ -1,20 +1,18 @@
 /**
  * Underscores.me specific javascript functionality
  */
- jQuery( function( $ ) {
- 	var advanced, simple,
- 		form = $('#generator-form');
- 	
-	advanced = $('<a href="#" class="generator-form-optionstoggle">Advanced Options</a>').appendTo('.generator-form-primary');
-	simple   = $('<a href="#" class="generator-form-optionstoggle">Simple Options</a>').appendTo('.generator-form-secondary');
- 	
- 	advanced.click( function( event ) {
-		form.removeClass('generator-form-skinny');
+document.onreadystatechange = function () {
+	var advanced = document.getElementsByClassName('generator-form-options-toggle')[0];
+	var simple = document.getElementsByClassName('generator-form-options-toggle')[1];
+	var form = document.getElementById('generator-form');
+	
+	advanced.addEventListener('click', function( event ) {
 		event.preventDefault();
- 	});
- 	
- 	simple.click( function( event ) {
- 		form.addClass('generator-form-skinny');
+		form.classList.remove('generator-form-skinny');
+	});
+	
+	simple.addEventListener('click', function( event ) {
 		event.preventDefault();
- 	});
- });
+		form.classList.add('generator-form-skinny');
+	});
+}
